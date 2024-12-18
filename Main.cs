0997f3a7c1fd9ad27d4f86aae88c5be5ef502442
Enrique Ramirez <enriquein@gmail.com>
2024-12-18 14:19:02 -0400

    Implement plugin logic. Release 1.0

---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Wox.Plugin;
+
+namespace DayOfYearWoxPlugin
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context)
+        {
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var day = DateTime.Now.DayOfYear.ToString();
+
+            var result = new Result
+            {
+                Title = "Day Of Year",
+                SubTitle = day
+            };
+
+            result.Action = e =>
+            {
+                Clipboard.SetText(day);
+                return true;
+            };
+
+            return new List<Result> { result };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DayOfYearWoxPlugin
+
 Simple plugin to get the day of the year. I used this a lot for work. Don't ask.
+
+Registers the `doy` keyword and copies the day of the year to the clipboard when selected.
